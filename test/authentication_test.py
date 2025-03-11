1cadb7d01dbb56805a24eee34e3b253b5e5373fb
Alexander Chechulin <bmw.f25@gmail.com>
2025-03-11 11:39:11 +0300

    Update authentication_test.py
    new

--- test/authentication_test.py
+++ test/authentication_test.py
@@ -6,7 +6,7 @@ from .marks import microservice, layer, owner, tm4j, jira_issues
 def test_should_authenticate_user_with_email():
     with allure.step("Open main page"):
         pass
-    with allure.step("Enter the creds"):
+    with allure.step("Enter the  creds"):
         with allure.step("username"):
             pass
     with allure.step("passwrod"):
